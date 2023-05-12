e87ca511629239221a238dc229992fc196bd1154
turbo1912 <yurtseven.gorkem@gmail.com>
2023-05-11 22:47:59 -0700

    fix: remove white-space css

--- src/pages/_navbar.tsx
+++ src/pages/_navbar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
               <span className="text-secondary font-normal">anything</span>
             </span>
           </a>
-          <span className="text-xs md:text-sm whitespace-nowrap font-light md:ms-1 max-md:ps-2 inline-block mr-6">
+          <span className="text-xs md:text-sm font-light md:ms-1 max-md:ps-2 inline-block">
             <span className="opacity-70">model inference provided by </span>
             <a
               className="link font-medium opacity-70 hover:opacity-100 transition-opacity duration-200"
