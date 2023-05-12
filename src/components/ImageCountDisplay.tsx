b8e9ec5e91d3b17d1c4975c8a13886e55988316b
gorkem <yurtseven.gorkem@gmail.com>
2023-05-11 18:06:54 -0700

    feat: make text larger (#20)

--- src/components/ImageCountDisplay.tsx
+++ src/components/ImageCountDisplay.tsx
@@ -8,12 +8,12 @@ export default function EmptyMessage(props: ImageCountDisplayProps) {
   return (
     <div className="text-center font-light prose prose-slate max-w-full my-4 md:my-8">
       <p>
-        Number of Images Created{" "}
         {props.count > 0 ? (
-          <CountUp start={props.count - 5} end={props.count} />
-        ) : (
-          ""
-        )}
+          <div>
+            Number of Images Created -
+            <CountUp start={props.count - 5} end={props.count} />
+          </div>
+        ) : ""}
       </p>
     </div>
   );
